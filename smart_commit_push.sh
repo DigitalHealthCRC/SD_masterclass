88251853a7a20be67f3d1e2c8cd22be0676fc70e
Amir <amir.marashi@gmail.com>
2026-03-24 13:07:36 +1100

    chore: remove temporary push helper

--- smart_commit_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-tr -d '\r' < /mnt/c/Users/Amir/.agents/skills/git-pushing/scripts/smart_commit.sh | \
-  sed 's/git commit -m "$MESSAGE"/git commit -m "$MESSAGE" || true/' > /tmp/smart_commit_push_inner.sh
-
-bash /tmp/smart_commit_push_inner.sh "$@"
